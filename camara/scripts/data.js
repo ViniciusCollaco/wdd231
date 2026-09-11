ac6f27be03efab8807dcef245b9949512e6f4cd2
Vinicius Collaço <viniciuscollaco@hotmail.com>
2026-09-11 15:54:49 -0300

    preparativos da atividade da semana

---
+++ camara/scripts/data.js
@@ -0,0 +1,2 @@
+document.querySelector('#anoAtual').textContent = new Date().getFullYear();
+document.querySelector('#ultimaModificacao').textContent = `Última Modificação: ${document.lastModified}`;
